227750c9ed58afc5e2339e2043a5a5bdad0f9bec
Amit Gupta <amitgupta.gwl@gmail.com>
2016-07-30 22:44:50 +0100

    add random function

--- os/nushi/stubbydb/functions.js
+++ os/nushi/stubbydb/functions.js
@@ -1,9 +1,7 @@
 var util = require('./util/util');
 
 exports.formatDate = function(dt_str,format){
-	console.log(dt_str)
 	var dt = util.formatDate(new Date(dt_str), format);
-	console.log(dt)
 	return dt;
 };
 
@@ -11,10 +9,21 @@ exports.urlEncode = function(url){
 	return encodeURI(url);
 };
 
-exports.random = function(N){
-		var num1 = Math.pow(10, N);
-		var num2 = Math.pow(10, N-1);
-		num1 = num1 - num2;
-		var randomnum = Math.floor(Math.random()*num1) + num2;
-		return randomnum;
-};
+var charsets = [];
+charsets['num'] = "0123456789";
+charsets['alpha'] = "abcdefghijklmnopqrstuvwxyz";
+charsets['alpha_num'] = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+exports.random = function(len,type){
+    var text = "";
+    var charset = "";
+    console.log(type);
+    if(!type)
+    	charset = charsets['num'];
+    else
+    	charset = charsets[type.toLowerCase()];
+    console.log(charset);
+    for( var i=0; i < len; i++ )
+        text += charset.charAt(Math.floor(Math.random() * charset.length));
+    return text;
+}
