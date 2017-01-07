5461a152d243ea905b4d105386953ce0353c41ae
Amit Gupta <amitgupta.gwl@gmail.com>
2017-01-07 16:46:43 +0000

    Fix Bug: date formatting

--- os/nushi/stubmatic/util/util.js
+++ os/nushi/stubmatic/util/util.js
@@ -38,24 +38,24 @@ var Days = [
 ];
 
 exports.formatDate = function(dt,format){
-  format = format.replace('yyyy', dt.getFullYear());
-  format = format.replace('YYYY', dt.getFullYear());
-  format = format.replace('yy', (dt.getFullYear()+"").substring(2));
-  format = format.replace('YY', (dt.getFullYear()+"").substring(2));
+  format = format.replace('ss', exports.pad(dt.getSeconds(),2));
+  format = format.replace('s', dt.getSeconds());
+  format = format.replace('dd', exports.pad(dt.getDate(),2));
+  format = format.replace('d', dt.getDate());
+  format = format.replace('mm', exports.pad(dt.getMinutes(),2));
+  format = format.replace('m', dt.getMinutes());
   format = format.replace('MMMM', monthNames[dt.getMonth()]);
   format = format.replace('MMM', monthNames[dt.getMonth()].substring(0,3));
   format = format.replace('MM', exports.pad(dt.getMonth()+1,2));
   format = format.replace('M', dt.getMonth()+1);
-  format = format.replace('dd', exports.pad(dt.getDate(),2));
-  format = format.replace('d', dt.getDate());
   format = format.replace('DD', Days[dt.getDay()]);
-  format = format.replace('D', Days[dt.getDay()].substring(0,3));
+  format = format.replace(/D(?!e)/, Days[dt.getDay()].substring(0,3));
+  format = format.replace('yyyy', dt.getFullYear());
+  format = format.replace('YYYY', dt.getFullYear());
+  format = format.replace('yy', (dt.getFullYear()+"").substring(2));
+  format = format.replace('YY', (dt.getFullYear()+"").substring(2));
   format = format.replace('HH', exports.pad(dt.getHours(),2));
   format = format.replace('H', dt.getHours());
-  format = format.replace('mm', exports.pad(dt.getMinutes(),2));
-  format = format.replace('m', dt.getMinutes());
-  format = format.replace('ss', exports.pad(dt.getSeconds(),2));
-  format = format.replace('s', dt.getSeconds());
   //format = format.replace(/"/g,'');
   return format;
 }
