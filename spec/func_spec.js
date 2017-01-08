15413685ed8b4b8351a190552ba04d3f64932c69
Amit Gupta <amitgupta.gwl@gmail.com>
2017-01-08 06:43:50 +0000

    Add unit tests

---
+++ spec/func_spec.js
@@ -0,0 +1,36 @@
+var formatDate =require('.././os/nushi/stubmatic/functions').formatDate;
+var random =require('.././os/nushi/stubmatic/functions').random;
+//var anyDateBetween =require('.././os/nushi/stubmatic/functions').anyDateBetween;
+
+
+describe("Functions ", function() {
+  var today = new Date();
+
+  it("formatDate should format date", function() {
+  	var result = formatDate(new Date(2016,11,2,23,45,23), "D DD MMM YYYY, HH:mm:ss");
+  	expect("Fri Friday Dec 2016, 23:45:23").toBe(result);
+  });
+
+  it("random should give random string of specified length ", function() {
+  	expect(random(3)).toMatch(/[0-9]{3}/);
+  	expect(0).toBe(random(-1).length);
+  	expect(random(3,'num')).toMatch(/[0-9]{3}/);
+  	expect(random(3,'alpha')).toMatch(/[a-z]{3}/);
+  	expect(random(5,'alpha_num')).toMatch(/[a-z0-9]{5}/);
+  });
+
+  /*it("anyDateBetween should give random date between 2 dates ", function() {
+  	var today = new Date();
+  	var pastDate = new Date();
+  	pastDate.setDate(pastDate.getDate - 5);
+
+  	var dt = anyDateBetween(pastDate,today);
+
+  	console.log(dt);
+  	expect(true).toBe(today > dt);
+  	expect(true).toBe(pastDate < dt);
+  	
+
+  });*/
+
+});
