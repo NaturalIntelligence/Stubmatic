f0fe604d8d59c0112895a32809a281d5197365f5
Amit Gupta <amitgupta.gwl@gmail.com>
2017-02-14 06:45:57 +0000

    Separate validator tests

--- spec/cli_spec.js
+++ spec/cli_spec.js
@@ -67,7 +67,6 @@ describe('CLI', function () {
         expect(server.start.calls.count()).toBe(1);
     });
 
-
     var buildServerOptions = climodule.__get__("buildServerOptions");
     describe('serveroptions', function () {
 
@@ -148,52 +147,5 @@ describe('CLI', function () {
 
     });
 
-    var  validate = climodule.__get__("validateSyntax");
-
-    describe('validator', function () {
-
-        beforeEach(() => {
-            spyOn(console,'log');
-        });
-
-        it('should give an error when file doesn\'t exist', function () {
-            validate("notexist");
-            expect(console.log).toHaveBeenCalledWith("Unsupported file");
-
-            validate("notexist.json");
-            expect(console.log).toHaveBeenCalledWith("Validation failed");
-        });
-
-        it('should validate JSON file', function () {
-            validate(path.join(__dirname, "test_assets/config.json"));
-            expect(console.log).toHaveBeenCalledWith("Validated successfully");
-        });
-
-        it('should validate YAML/YML file', function () {
-            validate(path.join(__dirname, "test_assets/mappings/mapping1.yaml"));
-            expect(console.log).toHaveBeenCalledWith("Validated successfully");
-        });
-
-        // it('should validate XML file', function () {
-
-        // });
-
-        it('should log error when invalid JSON file', function () {
-            validate(path.join(__dirname, "test_assets/files/invalidConfig.json"));
-            expect(console.log.calls.count()).toEqual(2);
-            expect(console.log.calls.argsFor(0)[0]).toEqual( "Validation failed");
-            //expect(console.log.calls.argsFor(1)[0]).toBe(jasmine.any(String));
-        });
-
-        it('should log error when invalid YAML/YML file', function () {
-            validate(path.join(__dirname, "test_assets/files/invalidmapping.yml"));
-            expect(console.log.calls.count()).toEqual(2);
-            expect(console.log.calls.argsFor(0)[0]).toEqual( "Validation failed");
-            expect(console.log.calls.argsFor(1)[0]).toEqual(jasmine.any(String));
-        });
-
-        // it('should log error when invalid XML file', function () {
-
-        // });
-    });
+    
 });
