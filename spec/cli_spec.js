e31898e3c200b485e322048a4e9a5ed5092b8dc8
Amit Gupta <amitgupta.gwl@gmail.com>
2017-03-04 07:07:48 +0000

    Bug fix

--- spec/cli_spec.js
+++ spec/cli_spec.js
@@ -13,7 +13,7 @@ describe('CLI', function () {
         cli(["node", "stubmatic", "--version" ,"tobeignored"]);
 
         expect(console.log.calls.count()).toEqual(1);
-        expect(console.log.calls.argsFor(0)[0]).toEqual( "5.0.1");
+        expect(console.log.calls.argsFor(0)[0]).toEqual( "5.0.3");
     });
 
     it('should log help', function () {
