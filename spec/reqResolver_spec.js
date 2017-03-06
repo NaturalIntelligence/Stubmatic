57b3ffdb05632750a6599cc1418bafd5fabfdc5b
Amit Gupta <amitgupta.gwl@gmail.com>
2017-03-06 14:29:52 +0000

    Commit all empty test files also

---
+++ spec/reqResolver_spec.js
@@ -0,0 +1,44 @@
+var config = require(".././lib/configbuilder");
+
+describe("Request resolver ", function() {
+
+  //match for URL
+  it("should match with correct mapping", function() {
+  });
+
+  //match for request body
+  it("should match with correct mapping against request body", function() {
+  });
+
+  //match for headers
+  it("should match with correct mapping against request headers", function() {
+  });
+
+  //match for query params
+  it("should match with correct mapping against query parameters", function() {
+  });
+
+
+});
+
+describe("Request resolver db sets::", function() {
+
+  //
+  it("should match if dbset presents", function() {
+  });
+
+  //
+  it("should match if key in dbset presents", function() {
+  });
+
+  //err:skip
+  //make it default
+  it("should not match if key in dbset doesn't present", function() {
+  });
+
+  //err: file
+  //depricate: instead
+  it("should match if key in dbset doesn't present", function() {
+  });
+
+});
