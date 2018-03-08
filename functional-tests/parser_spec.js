674c0b76fb75676c3f04a7271f97c307985619b1
Amit Gupta <amitgupta.gwl@gmail.com>
2018-03-08 13:30:26 +0530

    integrate nimn and msgpack
    including functional tests

---
+++ functional-tests/parser_spec.js
@@ -0,0 +1,62 @@
+if (!global.Promise) {
+  global.Promise = require('q');
+}
+
+var chai = require('chai')
+  , chaiHttp = require('chai-http');
+
+var rewire = require('rewire'),
+   cli = rewire(".././index").__get__("cli");
+
+ chai.use(chaiHttp);
+
+try{
+    cli(["node", "stubmatic", "-d","functional-tests/assets"/*, "-v"*/]);
+}catch(err){
+    console.log("Server is already started");
+}
+
+describe('FT', function () {
+
+    it('should return nimn data without date compression', function (done) {
+        chai.request("http://localhost:9999")
+            .get('/nimn')
+            .then(res => {
+                expect(res.status).toBe(200);
+                expect(res.text.length).toBe(33);
+                done();
+            }).catch( err => {
+                markFailed(err,fail,done);
+            });
+    });
+
+    it('should return nimn data with date compression', function (done) {
+        chai.request("http://localhost:9999")
+            .get('/nimn/withdate')
+            .then(res => {
+                expect(res.status).toBe(200);
+                expect(res.text.length).toBe(25);
+                done();
+            }).catch( err => {
+                markFailed(err,fail,done);
+            });
+    });
+
+    it('should return msgpack data', function (done) {
+        chai.request("http://localhost:9999")
+            .get('/msgpack')
+            .then(res => {
+                expect(res.status).toBe(200);
+                expect(res.text.length).toBe(54);
+                done();
+            }).catch( err => {
+                markFailed(err,fail,done);
+            });
+    });
+
+    
+});
+
+function markFailed(err,fail,done){
+    fail(err.message);
+}
