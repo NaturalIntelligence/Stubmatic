8af7a464e5f6f6e7a232ac489dfb1e107ed61df3
Amit Gupta <amitgupta.gwl@gmail.com>
2018-03-09 08:39:58 +0530

    modify tests to restore mapping config

--- spec/mappingsLoader_spec.js
+++ spec/mappingsLoader_spec.js
@@ -15,27 +15,7 @@ describe("Mapping Loader ", function() {
 		}
   	};
 
-  	var defaultMappingConfig = {
-  		"mappings": {
-		    "default": {
-		      "request": {
-		        "method": "POST",
-						"headers": {
-							"default": "header-value"
-						}
-		      },
-		      "response": {
-		        "strategy": "first-found",
-		        "latency": 100,
-		        "status": 500,
-						"headers": {
-							"default2": "header-value2"
-						}
-		      }
-		    },
-		    "files": [ path.join(__dirname ,"test_assets/mappings/mapping1.yaml"), path.join(__dirname ,"test_assets/mappings/mapping2.yaml")]
-		}
-  	}
+  	
 
   it("should load mappings from multipls files", function() {
   	var mappings = mappingLoader.buildMappings(onlyMappingConfig);
@@ -78,51 +58,87 @@ describe("Mapping Loader ", function() {
 
   });
 
-	//Preference: given mapping > default mapping > default values
-	//commented as it updates default config which makes other test fails
-  /* it("should attach default mappings otherwise missing mappings", function() {
-  	var mappings = mappingLoader.buildMappings(defaultMappingConfig);
-  	
-  	var expected = { 
-  		request: { url: '/url1/1', method: 'GET' , headers: { default: 'header-value' }},
-    	response: { file: 'someFile.txt', status: 500, latency: 100, headers: { default2: 'header-value2' }}
-    };
+	describe("with default mapping ", function() {
 
-		//console.log(JSON.stringify(mappings[0],null,4));
-  	expect(mappings[0]).toEqual(expected);
+		var defaultMappingConfig = {
+  		"mappings": {
+		    "default": {
+		      "request": {
+		        "method": "POST",
+						"headers": {
+							"default": "header-value"
+						}
+		      },
+		      "response": {
+		        "strategy": "first-found",
+		        "latency": 100,
+		        "status": 500,
+						"headers": {
+							"default2": "header-value2"
+						}
+		      }
+		    },
+		    "files": [ path.join(__dirname ,"test_assets/mappings/mapping1.yaml"), path.join(__dirname ,"test_assets/mappings/mapping2.yaml")]
+		}
+	}
 
-  	expected = { 
-  		request: { url: '/url1/2', method: 'HEAD' , headers: { default: 'header-value' }},
-    	response: { body: 'ok', status: 500, latency: 100, headers: { default2: 'header-value2' }}
-    };
+		let oldMapping;
+		beforeEach(function(){
+			oldMapping = mappingLoader.getMappings();
+		});
 
-  	expect(mappings[1]).toEqual(expected);
+		
+	
+		//Preference: given mapping > default mapping > default values
+		//commented as it updates default config which makes other test fails
+		it("should attach default mappings otherwise missing mappings", function() {
+			var mappings = mappingLoader.buildMappings(defaultMappingConfig);
+			
+			var expected = { 
+				request: { url: '/url1/1', method: 'GET' , headers: { default: 'header-value' }},
+				response: { file: 'someFile.txt', status: 500, latency: 100, headers: { default2: 'header-value2' }}
+			};
 
-	expected = { 
-  		request: { url: '/url1/3', method: 'PUT' , headers: { default: 'header-value' }},
-    	response: { status: 502, latency: 3000, headers: { default2: 'header-value2' }}
-    };
-  	expect(mappings[2]).toEqual(expected);
+			//console.log(JSON.stringify(mappings[0],null,4));
+			expect(mappings[0]).toEqual(expected);
 
-  	expected = { 
-  		request: { url: '/url1/4', method: 'POST', post: 'check this' , headers: { default: 'header-value' }},
-    	response: { files: ['someFile.txt','default.txt'], strategy: 'first-found', status: 500, latency: 100, headers: { default2: 'header-value2' }}
-    };
-  	expect(mappings[3]).toEqual(expected);
+			expected = { 
+				request: { url: '/url1/2', method: 'HEAD' , headers: { default: 'header-value' }},
+				response: { body: 'ok', status: 500, latency: 100, headers: { default2: 'header-value2' }}
+			};
 
-  	expected = { 
-  		request: { url: '/url2', method: 'POST' , headers:{custom: 'Custom'}},
-    	response: { body: 'This is sample contents served from body', status: 500, latency: [1000,2000], headers: { default2: 'header-value2' }}
-    };
-  	expect(mappings[4]).toEqual(expected);
+			expect(mappings[1]).toEqual(expected);
 
-  	expected = { 
-  		request: { url: '/url4', method: 'POST', headers: { default: 'header-value' }},
-  		dbset: { db: 'dbsetname', key: 'keyname'},
-    	response: { file: 'responseFilePath', status: 500, latency: 100, headers: { default2: 'header-value2' }}
-    };
-  	expect(mappings[6]).toEqual(expected);
+		expected = { 
+				request: { url: '/url1/3', method: 'PUT' , headers: { default: 'header-value' }},
+				response: { status: 502, latency: 3000, headers: { default2: 'header-value2' }}
+			};
+			expect(mappings[2]).toEqual(expected);
 
-  }); */
+			expected = { 
+				request: { url: '/url1/4', method: 'POST', post: 'check this' , headers: { default: 'header-value' }},
+				response: { files: ['someFile.txt','default.txt'], strategy: 'first-found', status: 500, latency: 100, headers: { default2: 'header-value2' }}
+			};
+			expect(mappings[3]).toEqual(expected);
+
+			expected = { 
+				request: { url: '/url2', method: 'POST' , headers:{custom: 'Custom'}},
+				response: { body: 'This is sample contents served from body', status: 500, latency: [1000,2000], headers: { default2: 'header-value2' }}
+			};
+			expect(mappings[4]).toEqual(expected);
+
+			expected = { 
+				request: { url: '/url4', method: 'POST', headers: { default: 'header-value' }},
+				dbset: { db: 'dbsetname', key: 'keyname'},
+				response: { file: 'responseFilePath', status: 500, latency: 100, headers: { default2: 'header-value2' }}
+			};
+			expect(mappings[6]).toEqual(expected);
+
+		});
+
+		afterEach(function(){
+			mappingLoader.setDefaultMappings(oldMapping);
+		});
+  });
 
 });
