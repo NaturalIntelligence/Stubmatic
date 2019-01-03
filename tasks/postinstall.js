3deb87e5ebbcbe2237b683e706c7b098a29cb93f
Amit Gupta <amitgupta.gwl@gmail.com>
2019-01-03 21:36:51 +0530

    update detail for release

--- tasks/postinstall.js
+++ tasks/postinstall.js
@@ -9,18 +9,9 @@ const light_green = "\033[1;32m";
 const light_blue = "\033[1;34m";
 const NC="\033[0m"; // No Color
 
-console.log(yellow+"              Donating to an open source project is more affordable"+NC);
-
-console.log('');
 console.log('                  ' + stars + ' Thank you for using stubmatic! ' + stars);
 console.log('');
-console.log('             Please consider donating to help us maintain this package');
-console.log('');
 console.log( light_blue + '                   https://www.patreon.com/bePatron?u=9531404' + NC);
-console.log('                                      or');
-console.log( light_blue + '   https://www.paypal.me/amitkumarguptagwl' + NC);
-console.log('');
-console.log('                                     ' + stars);
 
 console.log('');
 console.log('');
