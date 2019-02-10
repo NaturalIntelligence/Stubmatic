a180e6a9fa39b7d3ac1b9d7303d3dd3a9cf491c2
Amit Gupta <amitgupta.gwl@gmail.com>
2019-02-10 12:01:00 +0530

    fix #12 : support regex result for various nodejs versions

--- spec/cli_spec.js
+++ spec/cli_spec.js
@@ -13,7 +13,7 @@ describe('CLI', function () {
         cli(["node", "stubmatic", "--version" ,"tobeignored"]);
 
         expect(console.log.calls.count()).toEqual(1);
-        expect(console.log.calls.argsFor(0)[0]).toEqual( "5.2.2");
+        expect(console.log.calls.argsFor(0)[0]).toEqual( "5.4.0");
     });
 
     it('should log help', function () {
