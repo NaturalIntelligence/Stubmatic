7953ed8d9c6bc427410a2e8898c3e8f7b669baf4
Amit Gupta <amitgupta.gwl@gmail.com>
2019-10-04 07:08:05 +0530

    Extract file name from the URL

---
+++ spec/util_spec.js
@@ -0,0 +1,52 @@
+var util = require(".././lib/util/util");
+
+describe("Util fileNameFromUrl ", function() {
+
+    it("when no url", function() {
+        const result = util.fileNameFromUrl("")
+        expect(result.name).toEqual("");
+        expect(result.ext).toEqual("");
+    });
+
+    it("when only slash", function() {
+        const result = util.fileNameFromUrl("/")
+        expect(result.name).toEqual("");
+        expect(result.ext).toEqual("");
+    });
+
+    it("when only slash", function() {
+        const result = util.fileNameFromUrl("/")
+        expect(result.name).toEqual("");
+        expect(result.ext).toEqual("");
+    });
+
+    it("when extenstion", function() {
+        const result = util.fileNameFromUrl("/this/is/index.php")
+        expect(result.name).toEqual("index");
+        expect(result.ext).toEqual("php");
+    });
+
+    it("when no extenstion", function() {
+        const result = util.fileNameFromUrl("/this/is/indexphp")
+        expect(result.name).toEqual("indexphp");
+        expect(result.ext).toEqual("");
+    });
+
+    it("when no extenstion", function() {
+        const result = util.fileNameFromUrl("/this/is/indexphp")
+        expect(result.name).toEqual("indexphp");
+        expect(result.ext).toEqual("");
+    });
+
+    it("when extenstion with long name", function() {
+        const result = util.fileNameFromUrl("/this/is/veryVeryLongUnexpectedName.php")
+        expect(result.name).toEqual("veryVeryLongUnexpect");
+        expect(result.ext).toEqual("php");
+    });
+
+    it("when extenstion with long name", function() {
+        const result = util.fileNameFromUrl("/this/is/veryVeryLongUnexpectedName.php", 50)
+        expect(result.name).toEqual("veryVeryLongUnexpectedName");
+        expect(result.ext).toEqual("php");
+    });
+});
